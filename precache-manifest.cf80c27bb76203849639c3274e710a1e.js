1717bfeb30d47cf91086ae4d16e08ecb457d6eb3
Ali Komijani <akpa125@gmail.com>
2020-08-19 00:20:53 +0430

    Updates

---
+++ precache-manifest.cf80c27bb76203849639c3274e710a1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00b4b94badde08ef896b220c8ebcc8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49985aa9321cf0a8fe6b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b33069a8c98358ab417f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49985aa9321cf0a8fe6b",
+    "url": "/static/js/2.b3dbbb07.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.b3dbbb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33069a8c98358ab417f",
+    "url": "/static/js/main.1d9959fe.chunk.js"
+  },
+  {
+    "revision": "5111a3ee738fba108936",
+    "url": "/static/js/runtime-main.15177931.js"
+  }
+]);
